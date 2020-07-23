6f3cd2b9a66f7a433e7c85c1f91fdbed1d8e3877
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-07-23 14:42:56 +0300

    added game over panel & gitignore

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameMaster : MonoBehaviour
 {
-    void Start()
+    private const string GAME_SCENE_NAME = "Game";
+    private const string MAIN_MENU_SCENE_NAME = "MainMenu";
+
+    public void LoadGame()
     {
-        
+        SceneManager.LoadScene(GAME_SCENE_NAME);
     }
 
-    void Update()
+    public void PlayAgain()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(MAIN_MENU_SCENE_NAME);
     }
 }
